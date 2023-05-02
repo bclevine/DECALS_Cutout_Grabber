addf83a457ffaafb7a503a9dcb9a08ed59c0fa8e
bclevine <benjamin.c.levine@stonybrook.edu>
2023-05-02 15:03:35 -0400

    Minor improvements to formatting and readability

--- get_catalog.py
+++ get_catalog.py
@@ -4,65 +4,76 @@ import os
 import urllib.request as urlib
 import socket
 
+
 def argument_parser():
-    '''
+    """
     Function that parses the arguments passed while running a script
-    '''
-    result = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    """
+    result = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     # path to the config file with parameters and information about the run
-    result.add_argument('-c', dest='cutouts', type=str, default="catalogs.txt") 
-    result.add_argument('-d', dest='directory', type=str, default="catalogs.nosync") 
-    result.add_argument('-l', dest='layer', type=str, default="ls-dr9")
-    result.add_argument('-m', dest='multithread', type=bool, default=False)
+    result.add_argument("-c", dest="cutouts", type=str, default="catalogs.txt")
+    result.add_argument("-d", dest="directory", type=str, default="catalogs.nosync")
+    result.add_argument("-l", dest="layer", type=str, default="ls-dr9")
+    result.add_argument("-m", dest="multithread", type=bool, default=False)
 
     return result
 
+
 def download_catalog(cutout):
     ra, dec, size = cutout
-    ra_min = ra-size; ra_max = ra+size
-    dec_min = dec-size; dec_max = dec+size
+    ra_min = ra - size
+    ra_max = ra + size
+    dec_min = dec - size
+    dec_max = dec + size
 
-    outname = '/'+str(ra)+'_'+str(dec)+'.fits'
-    if not os.path.isfile(outputs+outname):
+    outname = f"/{ra}_{dec}.fits"
+    if not os.path.isfile(outputs + outname):
         try:
-            urlib.urlretrieve(url.format(args.layer, ra_min, ra_max, dec_min, dec_max),outputs+outname)
+            urlib.urlretrieve(
+                url.format(args.layer, ra_min, ra_max, dec_min, dec_max),
+                outputs + outname,
+            )
         except Exception as e:
-            print('Catalog at [', ra, dec, '] timed out. :(')
+            print("\x1b[31m Catalog at [", ra, dec, "] timed out. :( \x1b[0m")
             return None
 
-        print('Catalog at [', ra, dec, '] has been downloaded.')
+        print("\x1b[32m Catalog at [", ra, dec, "] has been downloaded. \x1b[0m")
     else:
-        print('Catalog at [', ra, dec, '] was already downloaded.')
+        print("\x1b[33m Catalog at [", ra, dec, "] was already downloaded. \x1b[0m")
 
 
-if __name__ == '__main__': 
-    
+if __name__ == "__main__":
     # read in command line arguments
     args = argument_parser().parse_args()
 
     # define all locations and variables
     directory = os.getcwd()
-    cutout_list = np.loadtxt(directory+'/'+args.cutouts)
+    cutout_list = np.loadtxt(f"{directory}/{args.cutouts}")
     if len(cutout_list) == 0:
         raise ValueError("No cutouts found in config file.")
 
-    if args.directory == None:
+    if args.directory is None:
         outputs = directory
     else:
-        outputs = directory+'/'+args.directory
+        outputs = f"{directory}/{args.directory}"
 
-    url = 'https://www.legacysurvey.org/viewer/{}/cat.fits?ralo={}&rahi={}&declo={}&dechi={}'
+    url = "https://www.legacysurvey.org/viewer/{}/cat.fits?ralo={}&rahi={}&declo={}&dechi={}"
 
     socket.setdefaulttimeout(30)
 
     # loop through each cutout and download the data
     if not args.multithread:
         for cutout in cutout_list:
-                    download_catalog(cutout)
+            download_catalog(cutout)
     else:
         from multiprocessing import Pool
+
+        # from tqdm.contrib.concurrent import process_map
+        # process_map(download_catalog, cutout_list)
         with Pool() as p:
             p.map(download_catalog, cutout_list)
-    
-    print('---------')
-    print('Done.')
+
+    print("---------")
+    print("Done.")
--- get_cutout.py
+++ get_cutout.py
@@ -3,49 +3,53 @@ import numpy as np
 import os
 import urllib.request as urlib
 
+
 def argument_parser():
-    '''
+    """
     Function that parses the arguments passed while running a script
-    '''
-    result = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    """
+    result = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     # path to the config file with parameters and information about the run
-    result.add_argument('-c', dest='cutouts', type=str, default="cutouts.txt") 
-    result.add_argument('-d', dest='directory', type=str, default="cutouts") 
-    result.add_argument('-l', dest='layer', type=str, default="ls-dr9")
-    result.add_argument('-s', dest='scale', type=float, default=0.262)
-    result.add_argument('-t', dest='type', type=str, default="fits")
+    result.add_argument("-c", dest="cutouts", type=str, default="cutouts.txt")
+    result.add_argument("-d", dest="directory", type=str, default="cutouts")
+    result.add_argument("-l", dest="layer", type=str, default="ls-dr9")
+    result.add_argument("-s", dest="scale", type=float, default=0.262)
+    result.add_argument("-t", dest="type", type=str, default="fits")
     return result
 
 
-if __name__ == '__main__': 
-    
+if __name__ == "__main__":
     # read in command line arguments
     args = argument_parser().parse_args()
 
     # define all locations and variables
     directory = os.getcwd()
-    cutout_list = np.loadtxt(directory+'/'+args.cutouts)
+    cutout_list = np.loadtxt(f"{directory}/{args.cutouts}")
     if len(cutout_list) == 0:
         raise ValueError("No cutouts found in config file.")
 
-    if args.directory == None:
+    if args.directory is None:
         outputs = directory
     else:
-        outputs = directory+'/'+args.directory
+        outputs = f"{directory}/{args.directory}"
 
-    if args.type == 'fits':
-        url = 'https://www.legacysurvey.org/viewer/fits-cutout?ra={}&dec={}&size={}&layer={}&pixscale={}&bands=grz'
-    elif args.type == 'jpeg':
-        url = 'https://www.legacysurvey.org/viewer/jpeg-cutout?ra={}&dec={}&size={}&layer={}&pixscale={}&bands=grz'
+    if args.type == "fits":
+        url = "https://www.legacysurvey.org/viewer/fits-cutout?ra={}&dec={}&size={}&layer={}&pixscale={}&bands=grz"
+    elif args.type == "jpeg":
+        url = "https://www.legacysurvey.org/viewer/jpeg-cutout?ra={}&dec={}&size={}&layer={}&pixscale={}&bands=grz"
     else:
         raise ValueError("Type must be either 'fits' (default) or 'jpeg'.")
 
     # loop through each cutout and download the data
     for cutout in cutout_list:
         ra, dec, size = cutout
-        outname = '/'+str(ra)+'_'+str(dec)+'.'+args.type
-        urlib.urlretrieve(url.format(ra, dec, int(size), args.layer, args.scale),outputs+outname)
-        print('Cutout at [', ra, dec, '] has been downloaded.')
-    
-    print('---------')
-    print('Done.')
+        outname = f"/{ra}_{dec}.{args.type}"
+        urlib.urlretrieve(
+            url.format(ra, dec, int(size), args.layer, args.scale), outputs + outname
+        )
+        print("Cutout at [", ra, dec, "] has been downloaded.")
+
+    print("---------")
+    print("Done.")
